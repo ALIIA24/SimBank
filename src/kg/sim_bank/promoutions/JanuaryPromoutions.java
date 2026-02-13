fa536a747f882e9ef8080684fa56c5c869d0a453
aliia24 <universe11.10.13@gmail.com>
2026-02-13 14:26:51 +0500

    4
    5

--- src/kg/sim_bank/promoutions/JanuaryPromoutions.java
+++ src/kg/sim_bank/promoutions/JanuaryPromoutions.java
@@ -16,5 +16,7 @@ public class JanuaryPromoutions {
         System.out.println("1");
         System.out.println("2");
         System.out.println("3");
+        System.out.println("4");
+        System.out.println("5");
     }
 }
