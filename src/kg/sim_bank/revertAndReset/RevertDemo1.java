3c37d7b40dddc60f45ec4dca89e27c35c2e23778
aliia24 <universe11.10.13@gmail.com>
2026-02-13 16:19:37 +0500

    Name

--- src/kg/sim_bank/revertAndReset/RevertDemo1.java
+++ src/kg/sim_bank/revertAndReset/RevertDemo1.java
@@ -3,5 +3,6 @@ package kg.sim_bank.revertAndReset;
 public class RevertDemo1 {
     public static void main(String[] args) {
         System.out.println("1");
+        System.out.println(My name is Aliia);
     }
 }
